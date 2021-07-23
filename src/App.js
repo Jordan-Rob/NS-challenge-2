06807a820c52f1f655a946b56dab91cafcf57a06
jordanrob709 <jordanrob709@gmail.com>
2021-07-23 07:27:28 +0300

    added state to handle input change

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
-
+import {useState} from 'react'
 
 function App() {
+  const [ albumID, setAlbumID ] = useState(null)
+
+  const handleChange = (event) => {
+    setAlbumID(event.target.value)
+    console.log(albumID)
+  }
+
+  const formHandler = (event) => {
+      event.preventDefault()
+  }
+
   return (
     <div className="App">
       <section>
         <div>
-          <form>
-            <input placeholder='Enter Id' type='number' required></input>
-            <button>Get Album Photos By ID</button>
+          <form  onSubmit={ formHandler }>
+            <input onChange= { handleChange } value={albumID} placeholder='Enter Id' type='number' required></input>
+            <button type="submit">Get Album Photos By ID</button>
           </form>
         </div>
       </section>
